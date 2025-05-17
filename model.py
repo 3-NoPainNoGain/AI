fdfcfae7a1ba8f7b9588dbb86b469944c41f5408
SooHee Choi <141746836+erika0915@users.noreply.github.com>
2025-05-18 04:01:53 +0900

    Feat : 수어 인식을 위한 1D-CNN 모델 구조 정의

---
+++ model.py
@@ -0,0 +1,25 @@
+import torch.nn as nn
+
+class SignLanguage1DCNN(nn.Module):
+    def __init__(self, num_classes):
+        super(SignLanguage1DCNN, self).__init__()
+        self.conv1 = nn.Conv1d(154, 64, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm1d(64)
+        self.conv2 = nn.Conv1d(64, 128, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm1d(128)
+
+        self.pool = nn.MaxPool1d(2)
+        self.dropout = nn.Dropout(0.5)
+
+        self.fc1 = nn.Linear(128 * 7, 128)  
+        self.fc2 = nn.Linear(128, num_classes)
+
+        self.activation = nn.LeakyReLU()
+
+    def forward(self, x):
+        x = x.permute(0, 2, 1)  
+        x = self.pool(self.activation(self.bn1(self.conv1(x))))
+        x = self.pool(self.activation(self.bn2(self.conv2(x))))
+        x = x.view(x.size(0), -1)
+        x = self.dropout(self.activation(self.fc1(x)))
+        return self.fc2(x)
